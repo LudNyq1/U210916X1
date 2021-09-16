661202384537d600c7858b6ccf1ec6b53ea653d1
Ludvig Nyqvist <ludvig.nyqvist1@outlook.com>
2021-09-16 12:15:27 +0200

    Initial push

---
+++ U210916X1/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace U210916X1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_MouseClick(object sender, MouseEventArgs e)
+        {
+            textBox3.Text = "Du heter " + textBox1.Text + " " + textBox2.Text;
+        }
+    }
+}
